09dbb05b3a925f48346f3a35b471769873d04139
unknown <eiza.majebi@pau.edu.ng>
2025-10-17 00:42:05 +0100

    (Practice two commit)

---
+++ week-3/practice_2/target/debug/deps/practice_2.d
@@ -0,0 +1,5 @@
+C:\Users\EIZA MAJEBI\Documents\e.majebiCOS101\week-3\practice_2\target\debug\deps\practice_2.d: src\main.rs
+
+C:\Users\EIZA MAJEBI\Documents\e.majebiCOS101\week-3\practice_2\target\debug\deps\practice_2.exe: src\main.rs
+
+src\main.rs:
---
+++ week-3/practice_2/target/debug/practice_2.d
@@ -0,0 +1,1 @@
+C:\Users\EIZA\ MAJEBI\Documents\e.majebiCOS101\week-3\practice_2\target\debug\practice_2.exe: C:\Users\EIZA\ MAJEBI\Documents\e.majebiCOS101\week-3\practice_2\src\main.rs
