c3be477025d83d995c73b7992c1a0cdacf34c712
unknown <eiza.majebi@pau.edu.ng>
2025-10-22 18:54:44 +0100

    (Pras

---
+++ week-4/practice_5/target/debug/deps/practice_5.d
@@ -0,0 +1,5 @@
+C:\Users\EIZA MAJEBI\Documents\e.majebiCOS101\week-4\practice_5\target\debug\deps\practice_5.d: src\main.rs
+
+C:\Users\EIZA MAJEBI\Documents\e.majebiCOS101\week-4\practice_5\target\debug\deps\practice_5.exe: src\main.rs
+
+src\main.rs:
