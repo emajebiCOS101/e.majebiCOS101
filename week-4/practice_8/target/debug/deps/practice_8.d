41d776cedb3dec0ca8d70bf46c948fdae8c78065
unknown <eiza.majebi@pau.edu.ng>
2025-10-22 19:47:48 +0100

    (Practice eight:Using loop and break)

---
+++ week-4/practice_8/target/debug/deps/practice_8.d
@@ -0,0 +1,5 @@
+C:\Users\EIZA MAJEBI\Documents\e.majebiCOS101\week-4\practice_8\target\debug\deps\practice_8.d: src\main.rs
+
+C:\Users\EIZA MAJEBI\Documents\e.majebiCOS101\week-4\practice_8\target\debug\deps\practice_8.exe: src\main.rs
+
+src\main.rs:
---
+++ week-4/practice_8/target/debug/practice_8.d
@@ -0,0 +1,1 @@
+C:\Users\EIZA\ MAJEBI\Documents\e.majebiCOS101\week-4\practice_8\target\debug\practice_8.exe: C:\Users\EIZA\ MAJEBI\Documents\e.majebiCOS101\week-4\practice_8\src\main.rs
