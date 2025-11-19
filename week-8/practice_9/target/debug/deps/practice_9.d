871ddae9e10afcc19b2ced38fa3591fec391a899
emajebiCOS101 <eiza.majebi@pau.edu.ng>
2025-11-19 23:38:02 +0100

    (Practice nine: Initializing and accessing tuples with or without data types)

---
+++ week-8/practice_9/target/debug/deps/practice_9.d
@@ -0,0 +1,5 @@
+C:\Users\izema\Documents\e.majebiCOS101\week-8\practice_9\target\debug\deps\practice_9.d: src\main.rs
+
+C:\Users\izema\Documents\e.majebiCOS101\week-8\practice_9\target\debug\deps\practice_9.exe: src\main.rs
+
+src\main.rs:
