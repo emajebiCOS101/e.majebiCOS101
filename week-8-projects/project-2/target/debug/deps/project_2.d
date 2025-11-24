265c85c906d21143ccac3a982a1570f46ab48778
emajebiCOS101 <eiza.majebi@pau.edu.ng>
2025-11-24 01:00:04 +0100

    (Project two)

---
+++ week-8-projects/project-2/target/debug/deps/project_2.d
@@ -0,0 +1,5 @@
+C:\Users\izema\Documents\e.majebiCOS101\week-8-projects\project-2\target\debug\deps\project_2.d: src\main.rs
+
+C:\Users\izema\Documents\e.majebiCOS101\week-8-projects\project-2\target\debug\deps\project_2.exe: src\main.rs
+
+src\main.rs:
---
+++ week-8-projects/project-2/target/debug/project-2.d
@@ -0,0 +1,1 @@
+C:\Users\izema\Documents\e.majebiCOS101\week-8-projects\project-2\target\debug\project-2.exe: C:\Users\izema\Documents\e.majebiCOS101\week-8-projects\project-2\src\main.rs
