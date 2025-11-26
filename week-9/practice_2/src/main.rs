d04544d7e15e923075c22bd5f3a8f747dbf5e5f5
emajebiCOS101 <eiza.majebi@pau.edu.ng>
2025-11-26 12:54:04 +0100

    Reading a file in rust using std::io::Read

--- week-9/practice_2/src/main.rs
+++ week-9/practice_2/src/main.rs
@@ -1,7 +1,7 @@
 use std::io::Read;
 
 fn main(){
-    let mut file = std::fs::File::create("welcome_message.txt").expect("create has failed");
+    let mut file = std::fs::File::create("welcome_message.txt").expect("create error");
     let mut file = std::fs::File:: open("welcome_message.txt").unwrap();
     let mut contents = String:: new();
     file.read_to_string(&mut contents).unwrap();
